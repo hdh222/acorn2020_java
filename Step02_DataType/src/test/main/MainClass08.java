d97cc9060266d55adc975e8aa4f45648c51cb29f
DH <ghdejrghks@gmail.com>
2020-05-28 16:21:40 +0900

    MainClass08.java 추가

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,18 @@
+package test.main;
+
+public class MainClass08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String type 의 메소드 사용해보기
+		String str = "abcde12345";				//string type
+		
+		int size = str.length();				//문자열의 길이
+		
+		char ch = str.charAt(5);				//5번째 인덱스의 문자 1개
+		
+		String result2 = str.toUpperCase();		//str을 대문자로 변경
+		
+	}
+
+}
