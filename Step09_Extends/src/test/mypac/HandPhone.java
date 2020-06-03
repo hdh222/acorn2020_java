66011db361c83fed0e9f6ddd9bb0c6be0f2fd3bc
DH <ghdejrghks@gmail.com>
2020-06-03 18:15:52 +0900

    Step09_Extends 추가

---
+++ Step09_Extends/src/test/mypac/HandPhone.java
@@ -0,0 +1,9 @@
+package test.mypac;
+
+public class HandPhone extends Phone{
+	
+	public void mobileCall() {
+		System.out.println("이동중에 전화를 걸다");
+	}
+
+}
---
+++ Step09_Extends/src/test/mypac/Phone.java
@@ -0,0 +1,9 @@
+package test.mypac;
+
+public class Phone extends Object{				//extends Object 생략가능
+	
+	//전화거는 메소드
+	public void call() {
+		System.out.println("전화");
+	}
+}
