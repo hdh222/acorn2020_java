34062373f8d442ef1b424744867eb8941b139cad
DH <ghdejrghks@gmail.com>
2020-06-15 18:19:57 +0900

    주석 오타 변경

--- Step12_GenericClass/src/test/mypac/FruitBox.java
+++ Step12_GenericClass/src/test/mypac/FruitBox.java
@@ -2,7 +2,7 @@ package test.mypac;
 /*
  * class 클래스명<Generic type>
  * 
- * 	- 클래스를 정의할 때 Generic type을 지정해서 특정 type을 종덕으로 지정할 수 있다.
+ * 	- 클래스를 정의할 때 Generic type을 지정해서 특정 type을 동적으로 지정할 수 있다.
  */
 public class FruitBox<T> {
 	//필드
